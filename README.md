58900cb42b4110c0f258f50b1176c01e1eb22072
ArtNazarov <programmist.nazarov@gmail.com>
2017-08-18 00:02:04 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # runchoco
+
+Easy to use tool for installing cholatey, post installation of essential and popular software
---
+++ main.pas
@@ -0,0 +1,89 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  AsyncProcess, ShellAPI;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AsyncProcess1: TAsyncProcess;
+    btChocoRun: TButton;
+    btChocoInstall: TButton;
+    btViewList: TButton;
+    btUpgrade: TButton;
+    Memo1: TMemo;
+    procedure btChocoRunClick(Sender: TObject);
+    procedure btChocoInstallClick(Sender: TObject);
+    procedure btUpgradeClick(Sender: TObject);
+    procedure btViewListClick(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+    procedure execbat();
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btChocoRunClick(Sender: TObject);
+var I :  Integer;  s : TStringList;
+begin
+   s:=TStringList.Create;
+   for I:=0 to memo1.lines.count-1 do
+    begin
+      s.add('choco install '+memo1.Lines[i]+' -Y');
+    end;
+   s.savetofile('temp.bat');
+   execbat;
+end;
+
+procedure TForm1.btChocoInstallClick(Sender: TObject);
+var s : TStringList;
+begin
+   s:=TStringList.create;
+   s.add('@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString(''https://chocolatey.org/install.ps1''))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"');
+   s.SaveToFile('temp.bat');
+   execbat;
+   s.free;
+end;
+
+procedure TForm1.btUpgradeClick(Sender: TObject);
+var s : TStringList;
+begin
+ s:=TStringList.create;
+ s.add('choco upgrade all');
+ s.SaveToFile('temp.bat');
+ execbat();
+ s.free();
+end;
+
+procedure TForm1.btViewListClick(Sender: TObject);
+begin
+   memo1.Visible:= not memo1.visible;
+end;
+
+procedure TForm1.execbat;
+var scmd1, scmd2 : String;
+begin
+      scmd1:='cmd';
+  scmd2:='/K temp.bat';
+  ShellExecute(form1.handle, PChar ('open'), PChar(scmd1), PChar(scmd2), nil, 1);
+   //deletefile('temp.bat');
+end;
+
+end.
+
